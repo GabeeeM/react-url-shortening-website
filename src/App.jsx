0aa22cfd3c7f053c6628d4f7add71c83280ee998
Gabriel Mack <69842642+GabeeeM@users.noreply.github.com>
2022-05-11 16:06:19 -0700

    init and set up axios fetch request
    set up initial data requesting component, and verified that it works. very happy i got it working already

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Shortener from "./components/Shortener";
+
+function App() {
+  return (
+    <div className="App flex flex-col justify-center items-center bg-slate-600 h-screen">
+      <Shortener />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Shortener.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+
+function Shortener() {
+  const [gurl, setGurl] = useState("hi there");
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput("");
+    fetchUrl();
+    console.log("fetching url");
+  };
+
+  const fetchUrl = async () => {
+    try {
+      console.log("trying");
+      let temp = "https://api.shrtco.de/v2/shorten?url=" + input;
+      let res = await axios(temp);
+      setGurl(res.data.result.full_short_link);
+    } catch (err) {
+    } finally {
+    }
+  };
+
+  return (
+    <div>
+      <h1>Hi everyone</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="url"
+          value={input}
+          onChange={(input) => setInput(input.target.value)}
+        />
+      </form>
+      <h2>{gurl}</h2>
+    </div>
+  );
+}
+
+export default Shortener;
